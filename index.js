3218e44bb504942472738f4603efddc2a6ec7454
杨凯 <906580223@qq.com>
2023-03-01 14:26:35 +0800

    动态获取文件

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const http = require('http')
+var url = require("url");
+var querystring = require("querystring");
+
 //设置主机名
 const hostName = '127.0.0.1';
 //设置端口
 const port = 8080;
 const server = http.createServer(function(req,res){
   res.setHeader('Content-Type','text/plain');
-  console.log(res.url)
-  parseUrl(req.url, res)
+  const arg = url.parse(req.url).query
+  const params = querystring.parse(arg)
+  console.log(params)
+  parseUrl(req.url, res, params)
 });
 server.listen(port,hostName,function(){
     console.log(`服务器运行在http://${hostName}:${port}`);
 });
 
 const apiMap = {
-  '/download': () => {
+  '/download': (params) => {
+    const { file_id } = params
     return {
       "code": 0,
       "data": {
-        "url": "https://cdn3.xbongbong.com/xbbProPrd/ding66041eb1c6df73f535c2f4657eb6378f/03023208291823807585/docx/167758131408356c403672a9e7c77dac35c1ba4c5bfaa.docx"
+        "url": `https://img2.rossroma.com/${file_id}.docx`
       }
     }
   },
@@ -49,47 +55,49 @@ const apiMap = {
       ]
     }
   },
-  '/upload': () => {
+  '/upload': (params) => {
+    const { file_id } = params
     return {
       "code": 0,
       "data": {
         "create_time": 1670218749,
         "creator_id": "405",
-        "id": "10",
+        "id": `${file_id.length % 10}`,
         "modifier_id": "405",
-        "modify_time": 1670328308,
-        "name": "167758131408356c403672a9e7c77dac35c1ba4c5bfaa.docx",
-        "size": 7556,
+        "modify_time": Date.now(),
+        "name": `${file_id}.docx`,
+        "size": file_id.length,
         "version": 10
       }
     }
   },
   'default': () => {
+    const { file_id } = params
     return {
       "code": 0,
       "data": {
         "create_time": 1670218749,
         "creator_id": "405",
-        "id": "10",
+        "id": `${file_id.length % 10}`,
         "modifier_id": "405",
-        "modify_time": 1670328308,
-        "name": "167758131408356c403672a9e7c77dac35c1ba4c5bfaa.docx",
-        "size": 7556,
+        "modify_time": Date.now(),
+        "name": `${file_id}.docx`,
+        "size": file_id.length,
         "version": 10
       }
     }
   }
 }
 
-function parseUrl(url, res) {
+function parseUrl(url, res, params) {
   let result = null
   for (const key in apiMap) {
     if (url.includes(key)) {
-      result = apiMap[key]()
+      result = apiMap[key](params)
     }
   }
   if (!result && url.includes('v3/3rd')) {
-    result = apiMap.default()
+    result = apiMap.default(params)
   }
   res.writeHead(200, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify(result))
